03efd70fe1ad6cb4a7b1c79c5471b7c48deecc32
nachatoui <91183220+nachatoui@users.noreply.github.com>
2022-06-01 18:03:50 +0200

    'test'

--- ReactECommerce/src/Pages/Product.jsx
+++ ReactECommerce/src/Pages/Product.jsx
@@ -131,7 +131,7 @@ const Product = () => {
   useEffect(() => {
     const getProduct = async () => {
       try {
-        const res = await publicRequest.get("/products/find/" + id);
+        const res = await publicRequest.get("https://insappes.herokuapp.com/api/products/find/" + id);
         setProduct(res.data);
       } catch {}
     };
